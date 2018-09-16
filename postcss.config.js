2fcfeb39f3d4ba14e28fb7e7c393ef9170d816f7
phung <phung@ryukyu-i.co.jp>
2018-09-16 10:22:05 +0700

    add plugin cssnano for CSS minifier.

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,13 @@ module.exports = {
     require('autoprefixer')({
       grid: true, 
       browsers: ['last 2 versions', 'ie 11']
-    }),
+    }), // add vendor prefixes
+    require('cssnano')({
+      preset: ['default', {
+        discardComments: {
+            removeAll: true,
+        },
+      }]      
+    }) // minify the result
   ]
 }
